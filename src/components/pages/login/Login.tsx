7e0de68fca401d8631de3c27dadbb27e43c2b615
Adam Makuchowski <maku971@gmail.com>
2023-10-20 06:14:19 +0200

    update layout login page

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,36 +1,24 @@
 import {
   LoginContainer,
   LoginBanner,
-  BannerWrapper,
-  CompanyNameWrapper,
-  MainDescriptionWrapper,
-  ReferencesWrapper,
   LoginFormWrapprt,
+  LogoContainer,
+  LogoImgWrapper,
 } from './LoginStyled'
-import CompanyName from './company_name/CompanyName'
-import MainDescription from './main_description/MainDescription'
-import HomeReferences from './home_references/HomeReferences'
 import LoginForm from './login_form/LoginForm'
 
 const Login = (): JSX.Element => {
   return (
     <LoginContainer>
-      <LoginBanner>
-        <BannerWrapper>
-          <CompanyNameWrapper>
-            <CompanyName />
-          </CompanyNameWrapper>
-          <MainDescriptionWrapper>
-            <MainDescription />
-          </MainDescriptionWrapper>
-          <ReferencesWrapper>
-            <HomeReferences />
-          </ReferencesWrapper>
-        </BannerWrapper>
-      </LoginBanner>
       <LoginFormWrapprt>
         <LoginForm />
       </LoginFormWrapprt>
+      <LoginBanner>
+        <LogoContainer>
+          <LogoImgWrapper src='/images/logo.png' alt="test" />
+        </LogoContainer>
+        
+      </LoginBanner>
     </LoginContainer >
   )
 }
